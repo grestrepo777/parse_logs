cb3ef3115653b6cc478648b05bcff054d8632482
grestrepo777 <122930865+grestrepo777@users.noreply.github.com>
2025-06-07 21:43:05 -0400

    parse_ssh_log.py

--- parse_ssh_log.py
+++ parse_ssh_log.py
@@ -4,22 +4,22 @@ import re
 from collections import Counter
 
 
-# load the file
-with open("auth.log", "r") as file: 
+
+with open("auth.log", "r") as file: # load the file 
     logs = file.readlines()
 
-pattern = r"Failed password.*from (\d+\.\d+\.\d+\.\d+)"
+pattern = r"Failed password.*from (\d+\.\d+\.\d+\.\d+)" # pattern the script searches for from the file, regex of the address
 
-ip_list=[]
+ip_list=[] # create a list to store IPs
 
 for line in logs: 
-    match = re.search(pattern, line)
+    match = re.search(pattern, line) # match the pattern going line by line
     if match: 
-        ip = match.group(1)
-        ip_list.append(ip)
+        ip = match.group(1) # returns whatever is inside the parantheses from pattern
+        ip_list.append(ip) # if there is a match, add the ip address to the created list ip_list
 
-ip_counts = Counter(ip_list)
+ip_counts = Counter(ip_list) # counts IP addresses
 
 print("Top 5 brute-force IPs: ")
-for ip, count in ip_counts.most_common(5): 
-    print(f"{ip}: {count} attempts")
+for ip, count in ip_counts.most_common(5): # top 5 most common ips in the list
+    print(f"{ip}: {count} attempts") 
